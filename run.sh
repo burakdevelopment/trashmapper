a172e9c306dd905d25e7ca7c3ece34d5c884082d
sh3llexpl01t <118610046+burakdevelopment@users.noreply.github.com>
2026-02-06 23:06:08 +0300

    Translate installation messages to English

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "♻️ TrashMapper V2 Kurulumu Başlıyor..."
+echo "♻️ TrashMapper V2 Installation Begins..."
 
 if [ ! -d "venv" ]; then
     python3 -m venv venv
-    echo "Sanal ortam oluşturuldu."
+    echo "A virtual environment has been created."
 fi
 
 source venv/bin/activate
@@ -15,7 +15,7 @@ pip install streamlit opencv-python-headless onnxruntime numpy matplotlib pyyaml
 
 #sudo apt install python3-libcamera python3-kmsdrm
 
-echo "✅ Kurulum Tamam."
-echo "🚀 Başlatılıyor... Tarayıcıdan http://localhost:8501 adresine girin."
+echo "✅ Installation OK."
+echo "🚀 Initializing... From browser http://localhost:8501 "
 
 streamlit run app.py --server.address 0.0.0.0
